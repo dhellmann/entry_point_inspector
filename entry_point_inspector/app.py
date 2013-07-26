26813408f8978cbe73545d62c5b0ba4b8a4b8b1e
Doug Hellmann <doug.hellmann@gmail.com>
2013-07-26 17:10:07 -0400

    start app

---
+++ entry_point_inspector/app.py
@@ -0,0 +1,28 @@
+import logging
+import sys
+
+from cliff import app
+from cliff import commandmanager
+
+import pkg_resources
+
+
+class EntryPointInspector(app.App):
+
+    log = logging.getLogger(__name__)
+
+    def __init__(self):
+        dist = pkg_resources.get_distribution('entry_point_inspector')
+        super(EntryPointInspector, self).__init__(
+            description='Tool for looking at the entry point plugins on a system',
+            version=dist.version,
+            command_manager=commandmanager.CommandManager('epi.commands'),
+        )
+
+
+def main(argv=sys.argv[1:]):
+    return EntryPointInspector().run(argv)
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
---
+++ setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+PROJECT = 'entry_point_inspector'
+
+# Change docs/source/conf.py too!
+VERSION = '0.1'
+
+from setuptools import setup, find_packages
+
+try:
+    long_description = open('README.rst', 'rt').read()
+except IOError:
+    long_description = ''
+
+setup(
+    name=PROJECT,
+    version=VERSION,
+
+    description='Tool for looking at the entry point plugins on a system',
+    long_description=long_description,
+
+    author='Doug Hellmann',
+    author_email='doug.hellmann@gmail.com',
+
+    url='https://github.com/dhellmann/entry_point_inspector',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+    ],
+
+    platforms=['Any'],
+
+    scripts=[],
+
+    provides=['entry_point_inspector',
+              ],
+    install_requires=[
+        'cliff',
+    ],
+
+    namespace_packages=[],
+    packages=find_packages(),
+    include_package_data=True,
+
+    entry_points={
+        'console_scripts': [
+            'epi = entry_point_inspector.app:main',
+        ],
+    },
+
+    zip_safe=False,
+)
